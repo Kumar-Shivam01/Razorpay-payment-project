e848ec362d87e2b3d541dddcc616021fa88eebd2
Kumar-Shivam01 <pkshivam9@gmail.com>
2026-09-21 00:15:39 +0530

    configure axios to use backend url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: 'http://localhost:3001/api/v1'
+    baseURL: 'https://razorpay-payment-project.onrender.com/api/v1' || 'http://localhost:3001/api/v1'
 })
 
 export default API;
